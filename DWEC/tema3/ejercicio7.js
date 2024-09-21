f47ace3ea65bde1003608eee8b3a199327513867
JavierPeiro <javierpeiroaguado@gmail.com>
2024-09-21 12:31:11 +0200

    javasript

--- DWEC/tema3/ejercicio7.js
+++
@@ -1,8 +0,0 @@
-let cantidad=prompt("Introduce how many lines of asterisk do you want:")
-
-if(isNaN(cantidad)==false && cantidad>0){
-    for(let i=0;i<cantidad;i++){
-        document.write("*")
-    }
-    document.write("*<br>")
-}
